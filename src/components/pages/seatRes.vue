bdc2776ec86f31740b494327dccdc0de365e570c
qiandaohujing <979423879@qq.com>
2019-06-05 21:15:28 +0800

    1

--- src/components/pages/seatRes.vue
+++ src/components/pages/seatRes.vue
@@ -9,7 +9,7 @@
 				:key="deskNum"
 				@click="seatReservation(item)"
 			>
-				<img src="../../assets/images/seat.png" />
+				<img src="../../assets/images/seat.png">
 				<div
 					class="mask"
 					:style="{
@@ -38,9 +38,9 @@
 			<div class="seatMessage">
 				<div>{{ title }}</div>
 				<div>{{ seatNum }}号自习座位</div>
-				<br />
+				<br>
 				<span>时间：</span>
-				<br />
+				<br>
 				<span>{{ getTime() }}--{{ getTimeLater() }}</span>
 			</div>
 			<span slot="footer" class="dialog-footer">
@@ -63,163 +63,175 @@
 </template>
 
 <script>
-import vheader from "../common/header";
-import vfooter from "../common/footer";
-import moment from "moment";
-import { showDesk } from "../../api/reservation";
-export default {
-	components: { vheader, vfooter },
-	data() {
-		return {
-			title: "",
-			seatNum: "",
-			showGone: false,
-			showExist: false,
-			checkTwice: false,
-			seatList: [
-				// {
-				// 	status: 1,
-				// 	index: 1,
-				// 	lock: false
-				// },
-				// {
-				// 	status: 1,
-				// 	index: 2,
-				// 	lock: false
-				// },
-				// {
-				// 	status: 1,
-				// 	index: 3,
-				// 	lock: false
-				// },
-				// {
-				// 	status: 0,
-				// 	index: 4,
-				// 	lock: false
-				// },
-				// {
-				// 	status: 0,
-				// 	index: 5,
-				// 	lock: true
-				// },
-				// {
-				// 	status: 0,
-				// 	index: 6,
-				// 	lock: true
-				// }
-			]
-		};
-	},
-	created() {
-		this.title = this.$route.params.title;
-		this.libLevel = this.$route.params.libLevel;
-		this.showDesk();
-	},
-	methods: {
-		showDesk() {
-			showDesk().then(res => {
-				console.log("res:", res);
-				// this.seatList = this.result.filter(item => {
-				// 	console.log("item.libLevel:", item.libLevel);
-				// 	console.log("this.libLevel:", this.libLevel);
-				// 	item.libLevel == this.libLevel;
-				// });
-				for (let i = 0; i < res.result.length; i++) {
-					const element = res.result[i];
-					console.log("element.libLevel:", element.libLevel);
-					console.log("this.libLevel:", this.libLevel);
-					if (element.libLevel == this.libLevel) {
-						this.seatList.push(element);
+	import vheader from "../common/header";
+	import vfooter from "../common/footer";
+	import moment from "moment";
+	import { showDesk } from "../../api/reservation";
+	import { date } from "../../api/reservation";
+	export default {
+		components: { vheader, vfooter },
+		data() {
+			return {
+				title: "",
+				seatNum: "",
+				showGone: false,
+				showExist: false,
+				checkTwice: false,
+				seatList: [
+					{
+						deskStatus: 1,
+						deskNum: 1,
+						lock: false
+					},
+					{
+						deskStatus: 1,
+						deskNum: 2,
+						lock: false
+					},
+					{
+						deskStatus: 1,
+						deskNum: 3,
+						lock: false
+					},
+					{
+						deskStatus: 0,
+						deskNum: 4,
+						lock: false
+					},
+					{
+						deskStatus: 0,
+						deskNum: 5,
+						lock: true
+					},
+					{
+						deskStatus: 0,
+						deskNum: 6,
+						lock: true
 					}
+				],
+				libLevel: ""
+			};
+		},
+		created() {
+			this.title = this.$route.params.title;
+			this.libLevel = this.$route.params.libLevel;
+			this.showDesk();
+		},
+		methods: {
+			showDesk() {
+				showDesk().then(res => {
+					console.log("res:", res);
+					// this.seatList = this.result.filter(item => {
+					// 	console.log("item.libLevel:", item.libLevel);
+					// 	console.log("this.libLevel:", this.libLevel);
+					// 	item.libLevel == this.libLevel;
+					// });
+					for (let i = 0; i < res.result.length; i++) {
+						const element = res.result[i];
+						console.log("element.libLevel:", element.libLevel);
+						console.log("this.libLevel:", this.libLevel);
+						if (element.libLevel == this.libLevel) {
+							this.seatList.push(element);
+						}
+					}
+					// let seatArr = res.result.filter(item => {
+					// 	item.libLevel == this.libLevel;
+					// });
+					// this.seatList = JSON.parse(JSON.stringify(seatArr));
+					console.log("seatList:", this.seatList);
+				});
+			},
+			handleClose() {
+				setTimeout(() => {
+					this.dialogVisible = false;
+				}, 100);
+			},
+			getTime() {
+				return moment().format("YYYY年M月D日 HH:mm:ss");
+			},
+			getTimeLater() {
+				return moment()
+					.add(4, "h")
+					.format("HH:mm:ss");
+			},
+			seatReservation(item) {
+				if (item.deskStatus) {
+					this.showGone = true;
+				} else {
+					this.seatNum = item.deskNum;
+					this.showExist = true;
 				}
-				// let seatArr = res.result.filter(item => {
-				// 	item.libLevel == this.libLevel;
-				// });
-				// this.seatList = JSON.parse(JSON.stringify(seatArr));
-				console.log("seatList:", this.seatList);
-			});
-		},
-		handleClose() {
-			setTimeout(() => {
-				this.dialogVisible = false;
-			}, 100);
-		},
-		getTime() {
-			return moment().format("YYYY年M月D日 HH:mm:ss");
-		},
-		getTimeLater() {
-			return moment()
-				.add(4, "h")
-				.format("HH:mm:ss");
-		},
-		seatReservation(item) {
-			if (item.deskStatus) {
-				this.showGone = true;
-			} else {
-				this.seatNum = item.deskNum;
-				this.showExist = true;
+			},
+			checkAgain() {
+				this.checkTwice = true;
+				this.showExist = false;
+			},
+			reservationSubmit() {
+				this.checkTwice = false;
+				const data = {
+					libLevel: this.libLevel,
+					deskNum: this.seatNum,
+					dateBegin: Date.parse(new Date()),
+					dateEnd: Date.parse(new Date())
+				};
+				console.log("data:", data);
+				date(data).then(res => {
+					console.log("res:", res);
+				});
 			}
-		},
-		checkAgain() {
-			this.checkTwice = true;
-			this.showExist = false;
-		},
-		reservationSubmit() {
-			this.checkTwice = false;
 		}
-	}
-};
+	};
 </script>
 
 <style scoped>
-#seatRes {
-	width: 100%;
-	/* background-color: #cccccc; */
-}
-.maincontain {
-	max-width: 1100px;
-	min-height: calc(100vh - 144px);
-	margin: 0 auto;
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
-	align-items: center;
-}
-.mask {
-	width: 220px;
-	height: 224px;
-	border-radius: 10px;
-	position: absolute;
-	margin-top: -224px;
-	z-index: 100;
-}
-.numberSigh {
-	font-size: 18px;
-	color: #414cddb7;
-	margin: 5px;
-}
-.item {
-	margin: 10px 50px;
-	border-radius: 15px;
-	overflow: hidden;
-}
-.item:hover {
-	cursor: pointer;
-	box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
-}
-.bodyMessage {
-	width: 100%;
-	text-align: center;
-	font-size: 18px;
-	font-weight: 600;
-	color: #ff3300;
-}
-.seatMessage {
-	width: 100%;
-	text-align: center;
-	font-size: 16px;
-}
-.seatMessage span {
-	margin: 12px;
-}
+	#seatRes {
+		width: 100%;
+		/* background-color: #cccccc; */
+	}
+	.maincontain {
+		max-width: 1100px;
+		min-height: calc(100vh - 144px);
+		margin: 0 auto;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		align-items: center;
+	}
+	.mask {
+		width: 220px;
+		height: 224px;
+		border-radius: 10px;
+		position: absolute;
+		margin-top: -224px;
+		z-index: 100;
+	}
+	.numberSigh {
+		font-size: 18px;
+		color: #414cddb7;
+		margin: 5px;
+	}
+	.item {
+		margin: 10px 50px;
+		border-radius: 15px;
+		overflow: hidden;
+	}
+	.item:hover {
+		cursor: pointer;
+		box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
+	}
+	.bodyMessage {
+		width: 100%;
+		text-align: center;
+		font-size: 18px;
+		font-weight: 600;
+		color: #ff3300;
+	}
+	.seatMessage {
+		width: 100%;
+		text-align: center;
+		font-size: 16px;
+	}
+	.seatMessage span {
+		margin: 12px;
+	}
 </style>
