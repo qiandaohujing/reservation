62539e06a1aa4d4cd68afeb7918435fc4fa38de0
qiandaohujing <43477065+qiandaohujing@users.noreply.github.com>
2019-08-01 15:19:11 +0800

    test 1

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,39 @@
 <template>
-	<div id="app">
-		<router-view></router-view>
-	</div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-	// import axios from "axios";
-	import { getToken } from "./utils/token";
+// import axios from "axios";
+import { getToken } from "./utils/token";
 
-	export default {
-		created() {
-			// axios
-			// 	.post(
-			// 		"https://www.easy-mock.com/mock/5c34a1dd571e3a7876576640/example/upload"
-			// 	)
-			// 	.then(res => {
-			// 		console.log("res:", res);
-			// 	});
-		},
-		mounted() {
-			// this.ifLogin();
-		},
-		methods: {
-			ifLogin() {
-				if (!getToken()) {
-					this.$router.push("/login");
-				}
-			}
-		}
-	};
+export default {
+  created() {
+    axios
+      .post(
+        "https://www.easy-mock.com/mock/5c34a1dd571e3a7876576640/example/upload"
+      )
+      .then(res => {
+        console.log("res:", res);
+      });
+  },
+  mounted() {
+    this.ifLogin();
+  },
+  methods: {
+    ifLogin() {
+      if (!getToken()) {
+        this.$router.push("/login");
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-	#app {
-		width: 100%;
-		height: 100%;
-	}
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
